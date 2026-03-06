9831949f900e7ade69aef89f969753fe45b8e4b9
syed-tp <syed@testpress.in>
2026-03-06 11:40:03 +0530

    feat: Add example usage of live-chat component

--- src/screens/VideoScreen.tsx
+++ src/screens/VideoScreen.tsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
-import { TPStreamsPlayerView } from 'react-native-tpstreams';
+import { TPStreamsPlayerView, TPStreamsLiveChat } from 'react-native-tpstreams';
 
 type VideoScreenProps = NativeStackScreenProps<RootStackParamList, 'Video'>;
 
 const VideoScreen: React.FC<VideoScreenProps> = ({ route }) => {
-    const { videoId, accessToken, startInFullscreen } = route.params;
+    const { videoId, accessToken, startInFullscreen, title } = route.params;
     const [isPlayerReady, setIsPlayerReady] = useState(false);
 
     // Delay player rendering to ensure screen layout is complete
@@ -30,6 +30,26 @@ const VideoScreen: React.FC<VideoScreenProps> = ({ route }) => {
                     />
                 )}
             </View>
+            {isPlayerReady && title === 'Live Video' && (
+                <TPStreamsLiveChat
+                    username="React-Native-User-External"
+                    roomId="734e0ec6-cc3d-40d0-8bcb-0f6374076225"
+                    title="Live Chat"
+                    colors={{
+                        primary: '#0040ffff', // Indigo
+                        background: 'rgba(255, 255, 255, 1)', // White
+                        text: '#000000ff', // Slate 900
+                        inputBackground: 'rgba(255, 255, 255, 1)', // Slate 100
+                        border: '#ffffffff', // Slate 200
+                    }}
+                    typography={{
+                        fontSize: 14,
+                        fontFamily: 'System', // Use system font for better performance
+                        fontWeight: '500',
+                    }}
+                    style={{ flex: 1 }}
+                />
+            )}
         </View>
     );
 };
